f7c5ef62149b27eba6bc261668e1789c2778f8a3
Jørgen Knudsen <jknuds1@hotmail.com>
2024-08-14 16:53:40 +0200

    Added addition game, but main funciton in while loop and added bool for quit.

--- MathGame/Program.cs
+++ MathGame/Program.cs
@@ -1,2 +1,111 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World! Testing.");
+﻿
+using System.Text;
+
+Console.WriteLine("Hello! Please enter your name:");
+Random Random = new Random();
+
+string? name = Console.ReadLine();
+bool quit = false;
+
+if (name == null)
+{
+    while (name == null)
+    {
+        Console.WriteLine("Incorrect entry. Please try again:");
+        string? newName = Console.ReadLine();
+        name = newName;
+    }
+    
+}
+
+name = name.ToUpper().Trim();
+
+while (quit == false)
+{
+    main(name);
+}
+
+
+
+void main(string name) 
+{
+    Console.WriteLine(@$"Hello {name} please choose on of the options below:
+                        A - Addition game
+                        S - Subtraction game
+                        M - Multiplication game
+                        D - Division game
+                        Q - quit");
+    string? userInput = Console.ReadLine(); 
+
+    switch (userInput.ToLower().Trim())
+    {
+        case "a":
+            AdditionGame();
+            break;
+
+        case "s":
+            SubtractionGame();
+            break;
+
+        case "m":
+            MultiplicationGame();
+            break;
+
+        case "d":
+            DivisionGame();
+            break;
+        case "q":
+            Console.WriteLine("Quitting..");
+            Environment.Exit(1);
+            quit = true;
+            break;
+    }
+}
+
+void AdditionGame() 
+{
+    Console.WriteLine("Addition game selected");
+    
+    decimal firstNumber = Random.Next(0,10);
+    decimal secondNumber = Random.Next(0,10);
+    decimal answer;
+
+    Console.WriteLine($"{firstNumber} + {secondNumber} = ");
+    string? answerInput = Console.ReadLine();
+
+    if (answerInput != null)
+    {
+        bool conversion = decimal.TryParse(answerInput, out answer);
+        decimal sum = firstNumber + secondNumber;
+
+        if (answer == (sum) && conversion)
+        {
+            Console.WriteLine("Correct answer!");
+        }
+        else
+        {
+            Console.WriteLine("Wrong answer.");
+        }
+    }
+    else 
+    {
+        Console.WriteLine("Wrong Input.");
+    }
+
+
+}
+
+void SubtractionGame() 
+{
+    Console.WriteLine("Subtraction game selected");
+}
+
+void MultiplicationGame() 
+{
+    Console.WriteLine("Multiplication game selected");
+}
+
+void DivisionGame()
+{
+    Console.WriteLine("Division game selected");
+}
